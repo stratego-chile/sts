e5229f8f4e98a099f9aad2dbb2cbe2f1b2fe03c4
KBeDevel <theuserbit@proton.me>
2023-05-12 13:55:29 -0400

    feat: app root page components

---
+++ src/app/layout.tsx
@@ -0,0 +1,49 @@
+import '@stratego-sts/styles/global.sass'
+
+import dynamic from 'next/dynamic'
+
+const DefaultFooter = dynamic(
+  () => import('@stratego-sts/components/layout/default-footer')
+)
+
+export const generateMetadata = () => {
+  return {
+    title: process.env.BRAND_NAME,
+    description: process.env.BRAND_DESCRIPTION,
+  }
+}
+
+const RootLayout = ({ children }: React.PropsWithChildren<WithoutProps>) => {
+  return (
+    <html lang="en" className="h-full bg-gray-50">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#212529" />
+        <meta name="theme-color" content="#212529" />
+      </head>
+      <body className="h-full flex flex-col">
+        <main className="flex flex-col flex-grow">{children}</main>
+        <DefaultFooter />
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ src/app/page.tsx
@@ -0,0 +1,44 @@
+import { getPageTitle } from '@stratego-sts/lib/format'
+import dynamic from 'next/dynamic'
+import Link from 'next/link'
+
+const Header = dynamic(
+  () => import('@stratego-sts/components/layout/default-header')
+)
+
+export const metadata = {
+  title: getPageTitle('Home'),
+  description: 'Welcome to Stratego Support Ticket System',
+}
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <div className="flex flex-grow items-center">
+        <div className="max-w-7xl w-full mx-auto">
+          <div className="p-6 space-y-8">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+              Welcome to Stratego Support Ticket System
+            </h1>
+            <p>
+              Please{' '}
+              <Link
+                href="/login"
+                className="items-center font-bold text-base text-blue-500 hover:text-blue-700"
+              >
+                log in
+              </Link>{' '}
+              to start using the system
+            </p>
+            <p>
+              If you don&apos;t have access to this platform, just contact us
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
