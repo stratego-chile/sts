36c7f3561b8ff58c4a75db4d5b585ca467f8ad74
KBeDevel <theuserbit@proton.me>
2023-05-12 17:45:23 -0400

    fix: get error output on Mongo connection
    Signed-off-by: KBeDevel <theuserbit@proton.me>

--- src/lib/db-connector.ts
+++ src/lib/db-connector.ts
@@ -1,12 +1,17 @@
 import { mongoConnector } from '@stratego-sts/lib/mongodb'
 
 export class Connector {
+  /**
+   * @server-side-only
+   */
   static async connect() {
     try {
       const mongoClient = await mongoConnector
 
       return mongoClient.db(process.env.MONGODB_NAME)
-    } catch {
+    } catch (error) {
+      console.error(error)
+
       return undefined
     }
   }
