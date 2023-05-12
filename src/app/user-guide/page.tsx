3692588bd5312a14fdf1ccb979722431e0125547
KBeDevel <theuserbit@proton.me>
2023-05-12 13:55:57 -0400

    feat: add user guide page

---
+++ src/app/user-guide/page.tsx
@@ -0,0 +1,19 @@
+const UserGuidePage = () => {
+  return (
+    <div>
+      <header className="bg-white border-b-[1px] border-b-gray-200">
+        <div className="flex flex-col gap-4 mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <span className="text-3xl font-bold tracking-tight text-gray-900">
+            User Guide
+          </span>
+        </div>
+      </header>
+
+      <main className="grid mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <p>Coming soon</p>
+      </main>
+    </div>
+  )
+}
+
+export default UserGuidePage
